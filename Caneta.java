d1cd35b75d95355a3b21a776575c7edcb2268514
Caio Godoy <c4io.gv.galvao@gmail.com>
2022-03-10 11:49:16 -0300

    Aula 4 part 2

--- Caneta.java
+++ Caneta.java
@@ -7,46 +7,18 @@ public class Caneta {
 	private int carga;
 	private boolean tampada;
 	
-	// metodo construtor
-	public Caneta(String m, String co, float p, int ca) {
-		this.setModelo(m);
-		this.cor = co;
-		this.setPonta(p);
-		this.carga = ca;
-		this.tampar();
+	public Caneta(String modelo, String cor, float ponta, int carga) {
+		super();
+		this.modelo = modelo;
+		this.cor = cor;
+		this.ponta = ponta;
+		this.carga = carga;
 	}
 	
-	public String getModelo() {
-		return this.modelo;
-	}
-	public void setModelo(String m) {
-		modelo = m;
-	}
-	public float getPonta() {
-		return this.ponta;
-	}
-	public void setPonta(float p) {
-		ponta = p;
-	}
-	
-	public void rabiscar() {
-		if(this.tampada == true) {
-			System.out.println("ERRO");
-		}
-		else if(this.tampada == false) {
-			System.out.println("Rabisco");
-		}
-	}
-	protected void tampar() {
-		this.tampada = true;
-	}
-	protected void destampar() {
-		this.tampada = false;
-	}
 	public void status() {
-		System.out.println("Modelo: " + getModelo());
+		System.out.println("Modelo: " + this.modelo);
 		System.out.println("Caneta: " + this.cor);
-		System.out.println("Ponta: " + getPonta());
+		System.out.println("Ponta: " + this.ponta);
 		System.out.println("Carga: " + this.carga);
 		System.out.println("Tampada: " + this.tampada);
 	}
