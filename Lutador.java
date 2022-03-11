478d8adfaa90eaa2e30254c0f50e11bae3e653bd
Caio Godoy <c4io.gv.galvao@gmail.com>
2022-03-11 09:16:13 -0300

    Aula 7

---
+++ Lutador.java
@@ -0,0 +1,137 @@
+package aulaPoo;
+
+public class Lutador {
+	private String nome;
+	private String nacionalidade;
+	private int idade;
+	private float altura;
+	private float peso;
+	private String categoria;
+	private int vitorias;
+	private int derrotas;
+	private int empates;
+	
+	//construct
+	public Lutador(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, 
+			int derrotas, int empates) {
+		super();
+		this.nome = nome;
+		this.nacionalidade = nacionalidade;
+		this.idade = idade;
+		this.altura = altura;
+		this.setPeso(peso);
+		this.vitorias = vitorias;
+		this.derrotas = derrotas;
+		this.empates = empates;
+	}
+	
+	//getters e setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+		this.setCategoria();
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	private void setCategoria() {
+		if (this.getPeso() < 52.2) {
+			this.categoria = "Inválido";
+		}
+		else if (this.getPeso() <= 70.3) {
+			this.categoria = "Leve";
+		}
+		else if (this.getPeso() <= 83.9) {
+			this.categoria = "Médio";
+		}
+		else if (this.getPeso() <= 120.2) {
+			this.categoria = "Pesado";
+		}
+		else {
+			this.categoria = "Inválido";
+		}
+	}
+	public int getVitorias() {
+		return vitorias;
+	}
+	public void setVitorias(int vitorias) {
+		this.vitorias = vitorias;
+	}
+	public int getDerrotas() {
+		return derrotas;
+	}
+	public void setDerrotas(int derrotas) {
+		this.derrotas = derrotas;
+	}
+	public int getEmpates() {
+		return empates;
+	}
+	public void setEmpates(int empates) {
+		this.empates = empates;
+	}
+	
+	public void apresentar() {
+		if (this.getCategoria() != "Inválido") {
+			System.out.println("--------------------------");
+			System.out.println("Lutador: " + this.getNome());
+			System.out.println("Origem: " + this.getNacionalidade());
+			System.out.println(this.getIdade() + " anos");
+			System.out.println(this.getAltura() + "m");
+			System.out.println("Peso : " + this.getPeso() + "kg");
+			System.out.println("Vitórias: " + this.getVitorias());
+			System.out.println("Derrotas: " + this.getDerrotas());
+			System.out.println("Empates: " + this.getEmpates());
+		}
+		else {
+			System.out.println("Inválido");
+		}
+	}
+	public void status() {
+		if (this.getCategoria() != "Inválido") {
+			System.out.println("--------------------------");
+			System.out.println(this.getNome());
+			System.out.println("Peso: " + this.getCategoria());
+			System.out.println(this.getVitorias() + " vitórias");
+			System.out.println(this.getDerrotas() + " derrotas");
+			System.out.println(this.getEmpates() + " empates");
+		}
+		else {
+			System.out.println("Inválido");
+		}
+	}
+	public void ganharLuta() {
+		this.setVitorias(getVitorias() + 1);
+	}
+	public void perderLuta() {
+		this.setDerrotas(getDerrotas() + 1);
+	}
+	public void empatarLuta() {
+		this.setEmpates(getEmpates() + 1);
+	}
+}
--- aulaPoo.java
+++ aulaPoo.java
@@ -2,14 +2,16 @@ package aulaPoo;
 
 public class aulaPoo {
 	public static void main(String[] args) {
-		ControleRemoto c = new ControleRemoto();
-		c.ligar();
-		c.maisVolume();
-		c.ligarMudo();
-		c.desligarMudo();
-		c.play();
-		c.desligar();
+		Lutador l[] = new Lutador[6];
+		l[0] = new Lutador("Lupin", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+		l[1] = new Lutador("João", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+		l[2] = new Lutador("Lance", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
+		l[3] = new Lutador("Druik", "Austria", 28, 1.93f, 81.6f, 13, 0, 2);
+		l[4] = new Lutador("José", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+		l[5] = new Lutador("John", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
 		
-		c.abrirMenu();
+		l[0].apresentar();
+		l[0].setPeso(10);
+		l[0].apresentar();
 	}
 }
